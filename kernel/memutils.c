f308ef8024b5f65d30948bacd78b6bc4162b0854
ExoCore-Kernel <jacktp2013@gmail.com>
2025-06-16 22:23:48 +0800

    Add serial module stub and strncmp implementation

--- kernel/memutils.c
+++ kernel/memutils.c
@@ -12,3 +12,15 @@ void *memset(void *dst, int val, size_t n) {
         d[i] = (unsigned char)val;
     return dst;
 }
+
+int strncmp(const char *s1, const char *s2, size_t n) {
+    for (size_t i = 0; i < n; i++) {
+        unsigned char c1 = s1[i];
+        unsigned char c2 = s2[i];
+        if (c1 != c2)
+            return c1 - c2;
+        if (c1 == '\0')
+            return 0;
+    }
+    return 0;
+}
