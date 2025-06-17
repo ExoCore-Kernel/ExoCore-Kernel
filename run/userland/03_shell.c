fb55eada58e90694f553552942bcba65563942b6
ExoCore-Kernel <jacktp2013@gmail.com>
2025-06-17 22:13:56 +0800

    Enhance shell and keyboard handling

--- run/userland/03_shell.c
+++ run/userland/03_shell.c
@@ -11,6 +11,15 @@ static char history[HIST_SIZE][MAX_INPUT];
 static int hist_count = 0;
 static int app_id = -1;
 
+static const char *boot_order[8] = {
+    "run00_kernel_tester",
+    "run_example",
+    "run_memtest",
+    "run02_kernel_tests",
+    "run03_shell"
+};
+static int boot_count = 5;
+
 static size_t strlen_s(const char *s) { size_t n=0; while(s[n]) n++; return n; }
 static void strcpy_s(char *d, const char *s) { while((*d++ = *s++)); }
 static int strncmp_s(const char *a, const char *b, size_t n) {
@@ -29,6 +38,23 @@ static void add_history(const char *cmd){
     else { for(int i=1;i<HIST_SIZE;i++) strcpy_s(history[i-1], history[i]); strcpy_s(history[HIST_SIZE-1], cmd); }
 }
 
+static void gfx_test(void){
+    volatile uint16_t *video = (uint16_t*)0xB8000;
+    for(int c=0;c<16;c++){
+        for(int i=0;i<80*25;i++)
+            video[i] = ((uint16_t)VGA_ATTR(c, VGA_BLACK) << 8) | ' ';
+        for(volatile int d=0; d<500000; d++);
+    }
+    for(int y=5;y<10;y++)
+        for(int x=10;x<20;x++)
+            video[y*80+x] = ((uint16_t)VGA_ATTR(VGA_LIGHT_RED, VGA_BLACK)<<8) | '#';
+    for(int y=12;y<17;y++)
+        for(int x=40;x<60;x++)
+            video[y*80+x] = ((uint16_t)VGA_ATTR(VGA_LIGHT_GREEN, VGA_BLACK)<<8) | '*';
+    for(volatile int d=0; d<1000000; d++);
+    console_clear();
+}
+
 static void show_history(int idx, char *buf, int *len){
     for(int i=0;i<*len;i++) console_backspace();
     strcpy_s(buf, history[idx]);
@@ -37,36 +63,34 @@ static void show_history(int idx, char *buf, int *len){
 }
 
 static void handle_command(const char *cmd){
-    if(!strncmp_s(cmd,"help",4)){
-        console_puts("Available: help, clear, halt, save, show, mem\n");
-    } else if(!strncmp_s(cmd,"clear",5)){
-        console_clear();
-    } else if(!strncmp_s(cmd,"halt",4)){
-        console_puts("Halting...\n");
-        for(;;) __asm__("hlt");
-    } else if(!strncmp_s(cmd,"save ",5)){
-        const char *msg = cmd + 5;
-        int idx = mem_save_app(app_id, msg, strlen_s(msg)+1);
-        if(idx >= 0){
-            console_puts("saved ");
-            console_udec(idx);
+    if(!strncmp_s(cmd,"gfxtst",6)){
+        gfx_test();
+    } else if(!strncmp_s(cmd,"elp",3)){
+        console_puts("Commands: gfxtst, elp, order, chngeordr\n");
+    } else if(!strncmp_s(cmd,"order",5)){
+        for(int i=0;i<boot_count;i++){
+            console_puts(boot_order[i]);
             console_putc('\n');
-        } else {
-            console_puts("save failed\n");
         }
-    } else if(!strncmp_s(cmd,"show ",5)){
-        int idx = atoi_s(cmd+5);
-        size_t sz; char *ptr = mem_retrieve_app(app_id, idx, &sz);
-        if(ptr){
-            console_puts(ptr);
-            console_putc('\n');
-        } else {
-            console_puts("invalid id\n");
+    } else if(!strncmp_s(cmd,"chngeordr",9)){
+        int idx=0;
+        const char *p = cmd+9;
+        while(*p && idx < 8){
+            while(*p==' ') p++;
+            if(!*p) break;
+            const char *start=p;
+            while(*p && *p!=' ') p++;
+            int len = p-start;
+            if(len>0){
+                int l = (len<31)?len:31;
+                char *tmp = mem_alloc_app(app_id, l+1);
+                if(!tmp) continue;
+                for(int i=0;i<l;i++) tmp[i]=start[i];
+                tmp[l]='\0';
+                boot_order[idx++] = tmp;
+            }
         }
-    } else if(!strncmp_s(cmd,"mem",3)){
-        console_puts("used: ");
-        console_udec(mem_app_used(app_id));
-        console_putc('\n');
+        boot_count = idx;
     } else if(strlen_s(cmd)>0){
         console_puts("Unknown command: ");
         console_puts(cmd);
@@ -88,13 +112,13 @@ void _start(){
             char c = console_getc();
             if(c=='\n'){ console_putc('\n'); break; }
             else if(c=='\b'){ if(len>0){ len--; console_backspace(); } }
-            else if((unsigned char)c==0x80){
+            else if((unsigned char)c==0x80){ /* left arrow: prev command */
                 if(hist_count){
                     if(hist_nav>0) hist_nav--;
                     show_history(hist_nav, buf, &len);
                 }
             }
-            else if((unsigned char)c==0x81){
+            else if((unsigned char)c==0x81){ /* right arrow: next command */
                 if(hist_count){
                     if(hist_nav < hist_count-1) hist_nav++;
                     else {
