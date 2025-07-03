3360f155216ba1f001a353ae67eca6132adc73ed
ExoCore-Kernel <jacktp2013@gmail.com>
2025-07-03 18:28:50 +0800

    Remove MicroPython build integration

--- kernel/micropython.c
+++ kernel/micropython.c
@@ -1,30 +1,13 @@
 #include "micropython.h"
-#include "console.h"
-#include "mem.h"
-#include "port/micropython_embed.h"
-#include <string.h>
-
-static char mp_heap[64 * 1024];
-
+// Stubs for removed MicroPython integration
 __attribute__((force_align_arg_pointer))
 void run_micropython(const char *code, size_t size) {
-    int stack_top;
-    mp_embed_init(mp_heap, sizeof(mp_heap), &stack_top);
-    char *buf = mem_alloc(size + 1);
-    if (!buf) {
-        mp_embed_deinit();
-        return;
-    }
-    memcpy(buf, code, size);
-    buf[size] = '\0';
-    mp_embed_exec_str(buf);
-    mp_embed_deinit();
+    (void)code;
+    (void)size;
 }
-__attribute__((force_align_arg_pointer))
 
+__attribute__((force_align_arg_pointer))
 void run_micropython_mpy(const uint8_t *buf, size_t size) {
-    int stack_top;
-    mp_embed_init(mp_heap, sizeof(mp_heap), &stack_top);
-    mp_embed_exec_mpy(buf, size);
-    mp_embed_deinit();
+    (void)buf;
+    (void)size;
 }
